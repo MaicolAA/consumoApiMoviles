44fe81d2a9403c67dcc60d2fca426f26d0351026
maicolaa <maicolaroyave10@gmail.com>
2024-04-02 23:03:57 -0500

    xx

--- app/src/main/java/com/example/kotlinconectionmysql/MainActivity.kt
+++ app/src/main/java/com/example/kotlinconectionmysql/MainActivity.kt
@@ -7,58 +7,64 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.android.volley.Request
+import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.kotlinconectionmysql.ui.theme.KotlinconectionmysqlTheme
-import org.json.JSONArray
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val queue = Volley.newRequestQueue(this)
-        var url = "http://localhost/apiMoviles0104/apiConection.php";
-
-        val stringRequest = StringRequest(Request.Method.GET, url, Response.Listener { response ->  }, Response.ErrorListener { error ->})
-        queue.add(stringRequest);
-
-        val jsonArray = JSONArray(response);
-
 
         setContent {
-            KotlinconectionmysqlTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MainActivityContent(queue)
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun MainActivityContent(queue: RequestQueue) {
+    KotlinconectionmysqlTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            var apiResult by remember { mutableStateOf("") }
 
+            LaunchedEffect(true) {
+                val url = "http://localhost/apiMoviles0104/apiConection.php"
 
+                val stringRequest = StringRequest(Request.Method.GET, url,
+                    Response.Listener { response ->
+                        apiResult = response
+                    },
+                    Response.ErrorListener { error -> })
 
+                queue.add(stringRequest)
+            }
 
+            Greeting(apiResult)
+        }
+    }
+}
+
+@Composable
+fun Greeting(apiResult: String, modifier: Modifier = Modifier) {
     Text(
-        text = "",
+        text = apiResult,
         modifier = modifier
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    KotlinconectionmysqlTheme {
-        Greeting("Android")
-    }
+fun DefaultPreview() {
+    MainActivityContent(Volley.newRequestQueue(LocalContext.current))
 }
-
